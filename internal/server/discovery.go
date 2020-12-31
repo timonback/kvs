4cfa244715a50846a5181911ba951cc3d30f45f3
Timon Back <timon.back+git@gmail.com>
2020-12-31 16:20:06 +0100

    Fix discovery check id request

--- internal/server/discovery.go
+++ internal/server/discovery.go
@@ -28,10 +28,14 @@ func StartServerDiscovery(arguments *arguments.Server) {
 		for peer := range arguments.Peers {
 			if _, ok := discovered[peer]; ok == false {
 				resp, err := http.Get("http://" + peer + context.HandlerPathInternalId)
-				body, _ := ioutil.ReadAll(resp.Body)
-				if err == nil && len(body) == context.DiscoveryIdLength {
-					internal.Logger.Printf("Discovered server at %s with id %s", peer, body)
-					discovered[peer] = time.Now()
+				if err == nil {
+					body, _ := ioutil.ReadAll(resp.Body)
+					if len(body) == context.DiscoveryIdLength {
+						internal.Logger.Printf("Discovered server at %s with id %s", peer, body)
+						discovered[peer] = time.Now()
+					} else {
+						internal.Logger.Printf("Invalid server id discovered at %s", peer)
+					}
 				} else {
 					internal.Logger.Printf("Invalid server discovered at %s", peer)
 				}
